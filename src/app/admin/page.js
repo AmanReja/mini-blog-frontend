511fc328aa2f1a360d95f7a76107ad1ea41ddd54
Aman Reja <amnmolla742@gmail.com>
2025-06-26 16:18:46 +0530

    fix

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -83,6 +83,7 @@ const Page = () => {
     } finally {
       setContent("");
       setTitle("");
+      setOpen(false);
     }
 
     console.log(title);
@@ -199,9 +200,6 @@ const Page = () => {
                 </button>
               ) : (
                 <button
-                  onClick={() => {
-                    setOpen(false);
-                  }}
                   className="bg-gray-900 text-white py-2 px-4 rounded hover:bg-gray-800 focus:outline-none focus:shadow-outline"
                   type="submit"
                 >
