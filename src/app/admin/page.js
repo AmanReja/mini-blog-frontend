a9434c71a7008ef131a7c07891a60cda71c38f8d
Aman Reja <amnmolla742@gmail.com>
2025-06-26 16:21:38 +0530

    fix

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -23,13 +23,25 @@ const Page = () => {
   };
 
   const handeldelete = async (post) => {
-    try {
-      dispatch(deletePost(post._id)).then(() => {
-        dispatch(getAllpost());
-      });
-    } catch (error) {
-      console.error(error);
-    }
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        try {
+          dispatch(deletePost(post._id)).then(() => {
+            dispatch(getAllpost());
+          });
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    });
   };
 
   const update = (post) => {
